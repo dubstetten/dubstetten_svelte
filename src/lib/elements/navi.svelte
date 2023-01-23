701cd828357d6fc0533fe586efe0c92c86e55b83
gobi <gabrielengelhart@gmx.at>
2023-01-23 22:22:25 +0100

    burger menu with svelteuidev

--- src/lib/elements/navi.svelte
+++ src/lib/elements/navi.svelte
@@ -1,44 +1,16 @@
 <script lang="ts">
-  import { fly } from "svelte/transition";
-  import Burger from "$lib/elements/Hamburger.svelte";
+  import { Burger } from "@svelteuidev/core";
+  let opened = false;
 
-  export let open = false;
-  export const onClick = (): void => {
-    open = !open;
-  };
+  import { Button, Divider, Menu } from "@svelteuidev/core";
+  import { Camera, ChatBubble, Gear } from "radix-icons-svelte";
 </script>
 
-<div class="burger">
-  <Burger {open} {onClick} />
-</div>
-{#if open}
-  <nav transition:fly={{ y: -200, duration: 400 }}>
-    <a href="/tickets">Tickets</a>
-    <a href="/lineup">Line-Up</a>
-    <a href="/about">Info</a>
-    <a href="/contact">Contact</a>
-  </nav>
-{/if}
-
-<style>
-  .burger {
-    display: flex;
-    justify-content: end;
-  }
-
-  nav {
-    position: absolute;
-    z-index: -1;
-    width: 100%;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    align-items: center;
-    background-color: var(--white);
-  }
-  a {
-    color: rgb(0, 0, 0);
-    text-decoration: none;
-  }
-</style>
+<Menu>
+  <Burger {opened} on:click={() => (opened = !opened)} slot="control"
+    >Toggle Menu</Burger
+  >
+  <Menu.Item>Settings</Menu.Item>
+  <Menu.Item>Messages</Menu.Item>
+  <Menu.Item>Gallery</Menu.Item>
+</Menu>
